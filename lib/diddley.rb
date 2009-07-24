a9b5a86cb9261574723f1a0e3859784ff041b105
Fredrik Mollerstrand <fredrik@mollerstrand.se>
2009-07-24 15:51:49 +0200

    ok, we have something working here. sending tweets remain.

--- lib/diddley.rb
+++ lib/diddley.rb
@@ -1,7 +1,11 @@
 require 'yaml'
+require 'rubygems'
+require 'json'
+
 
 class Diddley
   def initialize
+    @last_seen_followers = nil
     @config_file = 'config/botdiddley.yml'
     read_config
   end
@@ -30,6 +34,62 @@ class Diddley
   end
 
   def go!
-    puts 'here we go!'
+    report "bot diddley reporting for service at pid #{$$}."
+    while true
+      report "wha? checking #{@owner}'s followers."
+      check_followers
+      report "sleeping."
+      sleep(@interval * 60)
+    end
+  end
+
+  def report(msg)
+    puts "#{Time.now} #{msg}"
+  end
+
+  # checks if there are new followers or if a followers stopped following,
+  # formats and sends tweets to owner.
+  def check_followers()
+    fresh = fetch_followers
+    unless @last_seen_followers.nil?
+      new_followers = fresh - @last_seen_followers
+      unfollowers   = @last_seen_followers - fresh
+      report("new followers: "+new_followers.to_s) unless new_followers.empty?
+      report("unfollowers: "+unfollowers.to_s) unless unfollowers.empty?
+      msgs = []
+      msgs += format_followers(new_followers)
+      msgs += format_unfollowers(unfollowers)
+      send_tweets(msgs)
+    end
+    @last_seen_followers = fresh
+  end
+
+  # returns list of followers' ids
+  def fetch_followers()
+    auth = [@twitteruser, @twitterpass].join(":")
+    url = "http://twitter.com/statuses/followers.json?id=" + @owner
+    cmd = "curl -u #{auth} #{url}"
+    j = %x[#{cmd}]  # TODO: how to capture error condition?
+    #j = %x[cat tests/fixtures/followers.json] # local
+    followers = JSON.parse(j)
+    followers_ids = followers.map { |follower| follower['screen_name'] }
+    # TODO: if followers.length > 100, fetch again.
+    return followers_ids
+  end
+
+  def format_followers(list)
+    return [] if list.empty?
+    return list.map {|follower| "@#{@owner}: @#{follower} is now following you."}
+    # TODO: squeeze many followers into one msg.
+  end
+  def format_unfollowers(list)
+    return [] if list.empty?
+    return list.map {|unfollower| "@#{@owner}: @#{unfollower} is no longer following you."}
+  end
+
+  def send_tweets(msgs)
+    msgs.each do |msg|
+      report 'send tweet: ' + msg
+    end
   end
 end
