2dc36fcc1d4f01e48a7252657900fb48b3e184da
Fredrik Mollerstrand <fredrik@mollerstrand.se>
2009-11-17 14:21:18 +0100

    wrote tests for format_messages()

---
+++ lib/text_formatter.rb
@@ -0,0 +1,10 @@
+class TextFormatter
+  def initialize
+    
+  end
+  
+  def format_messages(names=[])
+    return [] if names.empty?
+    
+  end
+end
--- tests/test_text_formatter.rb
+++ tests/test_text_formatter.rb
@@ -1,2 +1,19 @@
 require 'helper'
+require 'text_formatter'
 
+class TestTextFormatter < Test::Unit::TestCase
+  def setup
+    @t = TextFormatter.new
+  end
+  def test_format_messages
+    # 
+    # format_messages() accepts a list of names.
+    # it should output a list of 
+    
+    assert_equal [], @t.format_messages([])
+    assert_equal "@james has stopped following you.",
+      @t.format_messages(['james'])
+    assert_equal "@anna, @klas och sven have stopped following you.",
+      @t.format_messages(['anna','klas','sven'])
+  end
+end
